94d693ad3df319d4ca90a457c3fd5138c7bd00eb
Fhillip Castillo <gerson.fhillip@gmail.com>
2020-05-05 15:03:34 -0400

    add readme base, and have it functional with a sample image

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ try:
 except ImportError:
     import Image
 import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract'
+# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 print(pytesseract.image_to_string(Image.open('bodyparts.jpeg')))
 # French text image to string
